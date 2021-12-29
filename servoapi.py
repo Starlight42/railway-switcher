a6ec0e6f1cb6fbda7f67200dbb669feecc8d49d8
Augustin Bernachon Irisarri <abernachon@gmail.com>
2021-12-29 23:02:45 +0100

    Added basic servo interface

---
+++ servoapi.py
@@ -0,0 +1,23 @@
+"""
+Augustin Bernachon 28/12/2021
+Railway Switcher Servo api
+"""
+
+from machine import PWM, Pin
+import gc
+
+class servoApi(object):
+  def __init__(self, servo_pin, servo_freq, servo_duty=30):
+    gc.collect()
+    self.pin = servo_pin
+    self.freq = servo_freq
+    self.duty = servo_duty
+    self.servo = PWM(Pin(self.pin), freq=self.freq, duty=self.duty)
+
+  def _get_duty(self):
+    return self.duty
+
+  def _set_duty(self, new_duty):
+    self.duty = new_duty
+    self.servo.duty(self.duty)
+
